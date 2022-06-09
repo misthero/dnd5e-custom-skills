749f4d98f3893421b0b7e0302d920819a38ff6a5
Luca T <misthero@users.noreply.github.com>
2022-06-09 14:14:26 +0200

    v1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 - v1.0.0: first public release.
 - v1.0.1: better compatibility with tidy5esheet
 - v1.0.2: fix i18n hook
+- v1.0.3: code fix
--- dnd5e-custom-skills.js
+++ dnd5e-custom-skills.js
@@ -1,5 +1,5 @@
 const MODULE_NAME = 'dnd5e-custom-skills';
-CONFIG.debug.hooks = true
+
 var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
   function adopt(value) { return value instanceof P ? value : new P(function(resolve) { resolve(value); }); }
   return new(P || (P = Promise))(function(resolve, reject) {
